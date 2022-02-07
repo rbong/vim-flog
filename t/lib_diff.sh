d3946dff9d95dd14183df6b208638c3b6a33a9b4
Roger Bongers <r.l.bongers@gmail.com>
2022-02-07 01:40:18 -0500

    Always color diffs in tests

--- t/lib_diff.sh
+++ t/lib_diff.sh
@@ -3,5 +3,5 @@ TEST_DIR=$(realpath -- "$(dirname -- "$0")")
 . "$TEST_DIR/lib_dir.sh"
 
 diff_data() {
-  diff --strip-trailing-cr -- "$1" "$DATA_DIR/$2"
+  diff --color --strip-trailing-cr -- "$1" "$DATA_DIR/$2"
 }
