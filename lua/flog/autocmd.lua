868abed88f06e3d3be925239caec2fde2ab12a73
rbong <r.l.bongers@gmail.com>
2024-08-22 18:02:55 -0400

    Fix highlighting in multiple graph windows

--- lua/flog/autocmd.lua
+++ lua/flog/autocmd.lua
@@ -5,9 +5,23 @@ local flog_hl = require("flog/highlight")
 
 local M = {}
 
-function M.nvim_create_graph_autocmds(buffer, instance_number, enable_graph)
+function M.nvim_init_hl_autocmd(group, buffer, instance_number)
   local winid = vim.fn.bufwinid(buffer)
 
+  local hl_cb = flog_hl.nvim_get_graph_hl_callback(buffer, winid, instance_number)
+  hl_cb({})
+
+  return vim.api.nvim_create_autocmd(
+    { "WinScrolled", "WinResized" },
+    {
+      callback = hl_cb,
+      group = group,
+      pattern = tostring(winid),
+    }
+  )
+end
+
+function M.nvim_create_graph_autocmds(buffer, instance_number, enable_graph)
   -- Create group and clear previous autocmds
   local group = vim.api.nvim_create_augroup("Floggraph", { clear = true })
 
@@ -15,31 +29,35 @@ function M.nvim_create_graph_autocmds(buffer, instance_number, enable_graph)
   vim.api.nvim_buf_clear_namespace(buffer, -1, 0, -1)
 
   if enable_graph and enable_graph ~= 0 then
-    -- Initialize highlighting
-    local hl_cb = flog_hl.nvim_get_graph_hl_callback(buffer, instance_number)
-    hl_cb({})
-
     -- Create autocmds
-    return {
-      vim.api.nvim_create_autocmd(
-        { "WinScrolled", "WinResized" },
-        {
-          callback = hl_cb,
-          group = group,
-          pattern = tostring(winid),
-        }
-      ),
-      vim.api.nvim_create_autocmd(
-        { "BufWipeout" },
-        {
-          buffer = buffer,
-          callback = function (ev)
-            flog_graph.clear_internal_graph_state(instance_number)
-          end,
-          group = group,
-        }
-      ),
-    }
+
+    M.nvim_init_hl_autocmd(group, buffer, instance_number)
+
+    vim.api.nvim_create_autocmd(
+      { "BufWipeout" },
+      {
+        buffer = buffer,
+        callback = function (ev)
+          flog_graph.clear_internal_graph_state(instance_number)
+        end,
+        group = group,
+      }
+    )
+
+    vim.api.nvim_create_autocmd(
+      { "WinEnter" },
+      {
+        buffer = buffer,
+        callback = function (ev)
+          if vim.fn.bufnr() == buffer then
+            M.nvim_init_hl_autocmd(group, buffer, instance_number)
+          end
+        end,
+        group = group,
+      }
+    )
+
+    return group
   end
 
   return {}
