35d413719158bf1e8157860d956cd6cc28559fa1
rbong <r.l.bongers@gmail.com>
2024-08-31 15:15:44 -0400

    Add ability to update test snapshots

--- t/lib_diff.sh
+++ t/lib_diff.sh
@@ -2,6 +2,17 @@ TEST_DIR=$(realpath -- "$(dirname -- "$0")")
 
 . "$TEST_DIR/lib_dir.sh"
 
-diff_data() {
+_diff_data() {
   diff --color --strip-trailing-cr -- "$1" "$DATA_DIR/$2"
+}
+
+diff_data() {
+  if [ "$UPDATE_SNAPSHOTS" = "true" ]; then
+    if ! _diff_data "$1" "$2" &>/dev/null; then
+      echo "WARNING: Updating snapshot: $2"
+      cp "$1" "$DATA_DIR/$2"
+    fi
+  fi
+
+  _diff_data "$1" "$2"
 }
