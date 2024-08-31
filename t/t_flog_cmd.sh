a30928535ae5caf85dfee872776290c6b0beedbf
rbong <r.l.bongers@gmail.com>
2024-08-31 16:47:31 -0400

    Add more command tests

--- t/t_flog_cmd.sh
+++ t/t_flog_cmd.sh
@@ -44,6 +44,46 @@ call flog#test#Assert('&filetype !=# "floggraph"')
 call flog#test#Assert('bufname() ==# "README.md"')
 EOF
 
+# Run :Flog in split
+run_vim_command <<EOF
+e README.md
+Flogsplit
+call flog#test#Assert('winnr("$") == 2')
+call flog#test#Assert('tabpagenr() == 1')
+call flog#test#Assert('&filetype ==# "floggraph"')
+call flog#test#Assert('bufname(bufwinnr(winnr("$"))) ==# "README.md"')
+call flog#test#Assert('getwinvar(winnr("$"), "&filetype") !=# "floggraph"')
+
+normal gq
+call flog#test#Assert('winnr("$") == 1')
+call flog#test#Assert('tabpagenr() == 1')
+call flog#test#Assert('&filetype !=# "floggraph"')
+call flog#test#Assert('bufname() ==# "README.md"')
+EOF
+
+# Open commit
+run_vim_command <<EOF
+Flog
+exec "normal \<CR>"
+
+call flog#test#Assert('winnr("$") == 2')
+call flog#test#Assert('&filetype ==# "floggraph"')
+call flog#test#Assert('getwinvar(winnr("$"), "&filetype") ==# "git"')
+EOF
+
+# Open commit in split
+run_vim_command <<EOF
+e README.md
+Flogsplit
+exec "normal \<CR>"
+
+call flog#test#Assert('winnr("$") == 3')
+call flog#test#Assert('&filetype ==# "floggraph"')
+call flog#test#Assert('getwinvar(2, "&filetype") ==# "git"')
+call flog#test#Assert('bufname(winbufnr(winnr("$"))) ==# "README.md"')
+call flog#test#Assert('getwinvar(winnr("$"), "&filetype") !=# "floggraph"')
+EOF
+
 # Run :Flog with fake output
 run_vim_command <<EOF
 Flog -rev=fake
