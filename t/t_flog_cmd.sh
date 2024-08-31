2f9aec4c984b071103c8042828c513154ef5944f
rbong <r.l.bongers@gmail.com>
2024-08-31 16:49:37 -0400

    Add logging to t_flog_cmd.sh

--- t/t_flog_cmd.sh
+++ t/t_flog_cmd.sh
@@ -16,7 +16,7 @@ cd "$WORKTREE"
 
 git_commit_tag 1-a
 
-# Run :Flog from empty tab
+echo "Test: Run :Flog from empty tab"
 run_vim_command <<EOF
 Flog
 call flog#test#Assert('winnr("$") == 1')
@@ -29,7 +29,7 @@ call flog#test#Assert('tabpagenr() == 1')
 call flog#test#Assert('&filetype !=# "floggraph"')
 EOF
 
-# Run :Flog from non-empty tab
+echo "Test: Run :Flog from non-empty tab"
 run_vim_command <<EOF
 e README.md
 Flog
@@ -44,7 +44,7 @@ call flog#test#Assert('&filetype !=# "floggraph"')
 call flog#test#Assert('bufname() ==# "README.md"')
 EOF
 
-# Run :Flog in split
+echo "Test: Run :Flog in split"
 run_vim_command <<EOF
 e README.md
 Flogsplit
@@ -61,21 +61,21 @@ call flog#test#Assert('&filetype !=# "floggraph"')
 call flog#test#Assert('bufname() ==# "README.md"')
 EOF
 
-# Open commit
+echo "Test: Open commit"
 run_vim_command <<EOF
 Flog
-exec "normal \<CR>"
+redir! >/dev/null | exec "normal \<CR>" | redir END
 
 call flog#test#Assert('winnr("$") == 2')
 call flog#test#Assert('&filetype ==# "floggraph"')
 call flog#test#Assert('getwinvar(winnr("$"), "&filetype") ==# "git"')
 EOF
 
-# Open commit in split
+echo "Test: Open commit in split"
 run_vim_command <<EOF
 e README.md
 Flogsplit
-exec "normal \<CR>"
+redir! >/dev/null | exec "normal \<CR>" | redir END
 
 call flog#test#Assert('winnr("$") == 3')
 call flog#test#Assert('&filetype ==# "floggraph"')
@@ -84,7 +84,7 @@ call flog#test#Assert('bufname(winbufnr(winnr("$"))) ==# "README.md"')
 call flog#test#Assert('getwinvar(winnr("$"), "&filetype") !=# "floggraph"')
 EOF
 
-# Run :Flog with fake output
+echo "Test: Run :Flog with fake output"
 run_vim_command <<EOF
 Flog -rev=fake
 
